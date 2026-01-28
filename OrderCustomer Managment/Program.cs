8347e02fc60c7ecab2d9ed19d0474dff163175e0
Jeisson Cardoso <101949185+Bemontx@users.noreply.github.com>
2026-01-28 11:59:56 -0500

    Update Program

--- OrderCustomer Managment/Program.cs
+++ OrderCustomer Managment/Program.cs
@@ -1,33 +1,43 @@
 using MediatR;
-using Microsoft.AspNetCore.Builder;
 using OrderCustomer_Managment.Infrastructure.DependencyInjections;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. controladores
+// --- SERVICIOS ---
+
+// 1. Controladores
 builder.Services.AddControllers();
 
-// 2. Swagger 
+// 2. Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// 3. Capa de Infraestructura 
 builder.Services.AddInfrastructure(builder.Configuration);
 
-// 4.  MediatR
+// 4. MediatR 
 builder.Services.AddMediatR(cfg => {
     cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
 });
 
 var app = builder.Build();
 
-// 5Middleware de Swagger
+// --- MIDDLEWARE (EL ORDEN IMPORTA) ---
+
+// 5. Activación de la interfaz de Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Order & Customer API V1");
+    });
 }
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
